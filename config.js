70307c6543915dd52b33dbf17e723b6a30a6a5ef
mswaroop <mswaroop@hotmail.com>
2023-08-02 11:07:19 +0530

    done

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   'linkedinAuth': {
-    'clientID': '77icus0acmfryt', // your App ID
-    'clientSecret': 'uG0cAq6W3gCkRFKx', // your App Secret
-    'callbackURL': 'https://hushhproto.vercel.app/auth/linkedin/callback'
+    'clientID': '', 
+    'clientSecret': '', 
+    //'callbackURL': 'https://hushhproto.vercel.app/auth/linkedin/callback'
+    'callbackURL': 'http://localhost:8081/auth/linkedin/callback'
   }
 }
